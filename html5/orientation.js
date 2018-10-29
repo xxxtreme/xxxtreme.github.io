f02ed2fe08041bbc928996dbba1f7076e15f82c1
xxxtreme <43954482+xxxtreme@users.noreply.github.com>
2018-10-29 15:42:13 +0200

    Add files via upload

--- html5/orientation.js
+++ html5/orientation.js
@@ -3,7 +3,7 @@ window.addEventListener("deviceorientation" , on_device_orientation);
 //-----------
 function  on_device_orientation(e)
 {
-	documnet.getElementById("id_alpha").innerHTML = e.alpha;
-	documnet.getElementById("id_beta").innerHTML = e.beta;
-	documnet.getElementById("id_agamma").innerHTML = e.gamma;
+	document.getElementById("id_alpha").innerHTML = e.alpha;
+	document.getElementById("id_beta").innerHTML = e.beta;
+	document.getElementById("id_agamma").innerHTML = e.gamma;
 }
