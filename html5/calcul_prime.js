1a65fe30589dc984efb462c6cf89845a0643a2a3
xxxtreme <43954482+xxxtreme@users.noreply.github.com>
2018-10-29 14:25:58 +0200

    Add files via upload

--- html5/calcul_prime.js
+++ html5/calcul_prime.js
@@ -1,4 +1,9 @@
-
+var stopped =false;
+onmessage = function(e){
+	if (e.data == "stop"){
+		stopped= true;
+	}
+}
 function is_prim(n)
 {
 	for (var i=2; i*i<= n; i++)
@@ -7,8 +12,19 @@ function is_prim(n)
 
 	return true;
 }
-for (var i = 1e9; i<1e10; i++)
+function calculeaza_prime(start_value)
+{
+	if (stopped)
+		;
+	else {
+for (var i = start_value; i<start_value + 1000; i++)
 	if (is_prim(i))
 		postMessage(i);
-
-postMessage("gata");
+	start_value +=1000;
+}
+	if (start_value < 1e10)
+setTimeout(calculeaza_prime, 1 , start_value);
+else
+	postMessage("gata");
+}
+calculeaza_prime(1e9);
