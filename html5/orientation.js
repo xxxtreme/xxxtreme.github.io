f2adc3da9fb8e41050f0e3ba36713a739ff817b8
xxxtreme <43954482+xxxtreme@users.noreply.github.com>
2018-11-05 13:36:01 +0200

    Add files via upload

--- html5/orientation.js
+++ html5/orientation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML="Logic version: 2018.10.11.05.1";
+document.getElementById("id_bussiness_version").innerHTML="Logic version: 2018.10.11.05.2";
 window.addEventListener("deviceorientation" , on_device_orientation);
 window.addEventListener("devicemotion" , on_device_motion);
 
@@ -21,8 +21,12 @@ function  on_device_orientation(e)
 function on_device_motion(e)
 {
 
-	document.getElementById("id_acc_z").innerHTML = e.accelerationIncludingGravity.z;
-	document.getElementById("id_acc_x").innerHTML = e.accelerationIncludingGravity.x;
-	document.getElementById("id_acc_y").innerHTML = e.accelerationIncludingGravity.y;
+	var acc_z = e.accelerationIncludingGravity.z;
+	var acc_x = e.accelerationIncludingGravity.x;
+	var acc_y = e.accelerationIncludingGravity.y;
+
+	document.getElementById("id_acc_z").innerHTML = Math.round(acc_z*100)/100;
+	document.getElementById("id_acc_x").innerHTML = Math.round(acc_x*100)/100;
+	document.getElementById("id_acc_y").innerHTML = Math.round(acc_y*100)/100;
 
 }
