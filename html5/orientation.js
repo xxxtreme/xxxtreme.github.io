990f5a5d3452e2de57519b926a50ed5f33d738ba
xxxtreme <43954482+xxxtreme@users.noreply.github.com>
2018-11-05 13:41:38 +0200

    Add files via upload

--- html5/orientation.js
+++ html5/orientation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML="Logic version: 2018.10.11.05.2";
+document.getElementById("id_bussiness_version").innerHTML="Logic version: 2018.10.11.05.3";
 window.addEventListener("deviceorientation" , on_device_orientation);
 window.addEventListener("devicemotion" , on_device_motion);
 
@@ -12,6 +12,7 @@ function  on_device_orientation(e)
 	var canvas = document.getElementById("id_canvas");
 	var context = canvas.getContext("2d");
 
+	let R=10;
 	context.beginPath();
 	context.arc(canvas.width/2 + e.gamma / 90 *(canvas.width/2 -R), canvas.height /2 + e.beta/90*(canvas.height/2-R),R,0,2*Math.PI);
 	canvas.stroke(); 
@@ -28,5 +29,8 @@ function on_device_motion(e)
 	document.getElementById("id_acc_z").innerHTML = Math.round(acc_z*100)/100;
 	document.getElementById("id_acc_x").innerHTML = Math.round(acc_x*100)/100;
 	document.getElementById("id_acc_y").innerHTML = Math.round(acc_y*100)/100;
+
+	document.getElementById("id_root_x").innerHTML = Math.round(Math.atan(acc_x / acc_z)* 100)/100;
+	document.getElementById("id_root_y").innerHTML = Math.round(Math.atan(acc_y / acc_z)* 100)/100;
 
 }
