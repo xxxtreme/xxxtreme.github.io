c8d7945055cadc366684075386b30e4df4d6a9c5
xxxtreme <43954482+xxxtreme@users.noreply.github.com>
2018-11-12 14:41:36 +0200

    Add files via upload

--- html5/cam.js
+++ html5/cam.js
@@ -1,6 +1,7 @@
-document.getElementById("id_bussiness_version").innerHTML="Logic version: 2018.11.12.2";
+document.getElementById("id_bussiness_version").innerHTML="Logic version: 2018.11.12.3";
 document.getElementById("id_button").addEventListener("click", start);
-
+document.getElementById("id_canvas").addEventListener("touchstart", on_touch);
+document.getElementById("id_canvas").addEventListener("mousedown", on_touch);
 
 //---------
 function on_success(e)
@@ -20,3 +21,10 @@ function start()
 	navigator.mediaDevices.getUserMedia(c).then(on_success).catch(on_failure);
 }
 //---------
+function on_touch(e)
+{
+	var canvas = document.getElementById("id_canvas");
+	var context = canvas.getContext("2d");
+	var video = document.getElementById("id_video");
+	context.drawImage(video, 0, 0);
+}
