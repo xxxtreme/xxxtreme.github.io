ee15e4d6fd649ff0aaf050faf90d49db0e378540
xxxtreme <43954482+xxxtreme@users.noreply.github.com>
2018-11-26 15:14:14 +0200

    Add files via upload

--- html5/touch.js
+++ html5/touch.js
@@ -1,10 +1,13 @@
-document.getElementById("id_bussiness_version").innerHTML="Logic version: 2018.11.26.5";
+document.getElementById("id_bussiness_version").innerHTML="Logic version: 2018.11.26.7";
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
 canvas.addEventListener("touchmove", on_touch_move);
 var canvas_bounding_rect = canvas.getBoundingClientRect();
 
 var last_pos = {x: 0, y:0};
+
+var last_pos_array = [];
+
 
 function on_touch_start(e)
 {
@@ -17,18 +20,26 @@ function on_touch_start(e)
 						10,
 						0 , 2*Math.PI);
 		context.stroke();
-			last_pos.x = e.changedTouches[i].pageX;
-			last_pos.y = e.changedTouches[i].pageY;
-			}
+			var last_pos = {x: e.changedTouches[i].pageX,
+							y:  e.changedTouches[i].pageY
+							id: e.changedTouches[i].identifier};
+
+			last_pos_array.push(last_pos);
 }
 
 function on_touch_move(e)
 {
 	e.preventDefault();
 	for (var i = 0; i < e.changedTouches.length; i++){
+		var j = 0;
+		for (var j = ; j< last_pos_array.length; j++)
+			if(last_pos_array[j].id == e.changedTouches[i].identifier)
+				break;
+
 		var context = canvas.getContext("2d");
 			context.beginPath();
-			context.moveTo(last_pos.x = canvas_bounding_rect.left , last_pos.y = canvas_bounding_rect.top);
+			context.lineWidth = 20;	
+			context.moveTo(last_pos_array[j].x = canvas_bounding_rect.left , last_pos_array[j].y = canvas_bounding_rect.top);
 			context.lineTo(e.changedTouches[i].pageX - canvas_bounding_rect.left,
 						e.changedTouches[i].pageY - canvas_bounding_rect.top);
 			context.stroke();
@@ -40,7 +51,7 @@ function on_touch_move(e)
 						10,
 						0 , 2*Math.PI);
 		context.stroke();
-			last_pos.x = e.changedTouches[i].pageX;
-			last_pos.y = e.changedTouches[i].pageY;
+			last_pos_array[j].x = e.changedTouches[i].pageX;
+			last_pos_array[j].y = e.changedTouches[i].pageY;
 	}
 }
