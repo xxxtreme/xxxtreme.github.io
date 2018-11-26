69cf05191dc87f89624ad1b59ec021929f6b54ae
xxxtreme <43954482+xxxtreme@users.noreply.github.com>
2018-11-26 14:39:31 +0200

    Add files via upload

--- html5/touch.js
+++ html5/touch.js
@@ -3,6 +3,7 @@ canvas.addEventListener("touchstart", on_touch_start);
 canvas.addEventListener("touchmove", on_touch_move);
 var canvas_bounding_rect = canvas.getBoundingClientRect();
 
+var last_pos = {x: 0, y:0};
 
 function on_touch_start(e)
 {
@@ -15,7 +16,9 @@ function on_touch_start(e)
 						10,
 						0 , 2*Math.PI);
 		context.stroke();
-	}
+			last_pos.x = e.changedTouches[i].pageX;
+			last_pos.y = e.changedTouches[i].pageY;
+			}
 }
 
 function on_touch_move(e)
@@ -24,10 +27,16 @@ function on_touch_move(e)
 	for (var i = 0; i < e.changedTouches.length; i++){
 		var context = canvas.getContext("2d");
 			context.beginPath();
+			context.moveTo(last_pos.x = canvas_bounding_rect.left , last_pos.y = canvas_bounding_rect.top);
+			context.lineTo(e.changedTouches[i].pageX - canvas_bounding_rect.left,
+						e.changedTouches[i].pageY - canvas_bounding_rect.top);
+
 			context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left,
 						e.changedTouches[i].pageY - canvas_bounding_rect.top,
 						10,
 						0 , 2*Math.PI);
 		context.stroke();
+			last_pos.x = e.changedTouches[i].pageX;
+			last_pos.y = e.changedTouches[i].pageY;
 	}
 }
