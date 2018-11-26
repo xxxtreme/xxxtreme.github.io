e80a89a14b61378ac900b0fb9be544077b211533
xxxtreme <43954482+xxxtreme@users.noreply.github.com>
2018-11-26 14:21:23 +0200

    Add files via upload

--- html5/touch.js
+++ html5/touch.js
@@ -1,6 +1,8 @@
-document.getElementById("id_bussiness_version").innerHTML="Logic version: 2018.11.26.0";
+document.getElementById("id_bussiness_version").innerHTML="Logic version: 2018.11.26.1";
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
+var canvas_bounding_rect = canvas.getBoundingClientRect();
+
 
 function on_touch_start(e)
 {
@@ -8,8 +10,8 @@ function on_touch_start(e)
 	for (var i = 0; i < e.changedTouches.length; i++){
 		var context = canvas.getContext("2d");
 			context.beginPath();
-			context.arc(e.changedTouches[i].pageX,
-						e.changedTouches[i].pageY,
+			context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left,
+						e.changedTouches[i].pageY - canvas_bounding_rect.top,
 						10,
 						0 , 2*Math.PI);
 		context.stroke();
